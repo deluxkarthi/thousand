dd8ea6eb46d93c5ffa2ac5de0472df75eea38da6
deluxkarthi <35652981+deluxkarthi@users.noreply.github.com>
2018-02-06 16:37:52 +0530

    Create thou.c

---
+++ thou.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    a=1000;
+    scanf("%d",&a);
+    if(a=1000)
+    {
+        printf("given number is prime",a);
+    }
+    else
+    {
+        printf("given number is not prime");
+    }
+}
